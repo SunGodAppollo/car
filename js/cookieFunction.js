2691f46b2f79c3c4e9158cabb0bd86cf932a7114
公司 <417045639@qq.com>
2018-11-14 16:48:01 +0800

    2018年11月14日 16:47:58

--- js/cookieFunction.js
+++ js/cookieFunction.js
@@ -7,11 +7,32 @@ document.cookie=key+"="+val;
 function cookieGet(key){
 
 }
+
+function getCookie(c_name)
+{
+if (document.cookie.length>0)
+  {
+  c_start=document.cookie.indexOf(c_name + "=")
+  if (c_start!=-1)
+    {
+    c_start=c_start + c_name.length+1
+    c_end=document.cookie.indexOf(";",c_start)
+    if (c_end==-1) c_end=document.cookie.length
+    return unescape(document.cookie.substring(c_start,c_end))
+    }
+  }
+return ""
+}
+
 //衍生------------------------------
 
 function cookieUser(val){
 console.log(val);
 document.cookie="id"+"="+val.id;
+document.cookie="nickName"+"="+val.nickName;
+document.cookie="image"+"="+val.image;
+document.cookie="phone"+"="+val.phone;
+
 
 }
 
