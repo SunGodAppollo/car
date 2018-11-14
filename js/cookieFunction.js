48db990469e9f87d779a79c49d77131c57fe54fd
公司 <417045639@qq.com>
2018-11-14 16:28:10 +0800

    2018年11月14日 16:28:08

--- js/cookieFunction.js
+++ js/cookieFunction.js
@@ -1,9 +1,18 @@
 //对cookie的操作
-
+//添加
 function cookieAdd(key,val){
-
+document.cookie=key+"="+val;
 }
-
+//得到
 function cookieGet(key){
 
 }
+//衍生------------------------------
+
+function cookieUser(val){
+console.log(val);
+document.cookie="id"+"="+val.id;
+
+}
+
+//
